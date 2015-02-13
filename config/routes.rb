10dbeafa99a3c4457ce597cfa210b57006c0a89e
Alex Lang <langalex4@gmail.com>
2015-02-13 10:00:12 -0600

    basic styling of login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new', as: :login
 
+  resources :users, only: [:create, :new, :edit, :show] 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
