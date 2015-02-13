5a7a34987b08d42441780e8cacd93bd3811d04d6
Alex Lang <langalex4@gmail.com>
2015-02-13 10:40:43 -0600

    error message on incorrect password or email submission

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,13 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		
+		user = User.find_by(email: params[:email])
+		if user && user.password == params[:password]
+			session[:user_id] = user.id
+		else
+			@error = "Incorrect email and/or password"
+			render :new
+		end
 	end
 
 end
