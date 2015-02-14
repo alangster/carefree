9a951763ff39205f603ebbeac48a3311fbceabd7
Alex Lang <langalex4@gmail.com>
2015-02-14 06:06:31 -0600

    officemailer now sends emails, issue was with password and then gmail login. unsure of whether or not it passes spec, but do not think so

--- app/controllers/admin/offices_controller.rb
+++ app/controllers/admin/offices_controller.rb
@@ -1,7 +1,23 @@
 class Admin::OfficesController < AdminController
 
 	def create
-		
+		p "DINGGGG"
+		@office = Office.new(new_office_params)
+		@company = Company.find(params[:company_id])
+		@office.company = @company
+		if @office.save!
+			p "SAVED"
+			OfficeMailer.new_office_signup(@office).deliver
+			redirect_to @company
+		else
+			render 'company/show'
+		end
+	end
+
+	private
+
+	def new_office_params
+		params.require(:office).permit(:phone, :street_address, :zip_code, :state_abbr, :contact_email)
 	end
 
 end
--- app/mailers/office_mailer.rb
+++ app/mailers/office_mailer.rb
@@ -1,6 +1,8 @@
 class OfficeMailer < ActionMailer::Base
 
   def new_office_signup(office)
+  	p "DING"
+  	@office = office
   	mail(to: office.contact_email, subject: 'Join Carefree', from: 'welcome@carefree.lv')
   end
 
