98a323b1ba2c9d8787ae4120ebc807d198029466
Alex Lang <langalex4@gmail.com>
2015-02-14 11:46:07 -0600

    added setup for office mailer specs

--- spec/mailers/office_mailer_spec.rb
+++ spec/mailers/office_mailer_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe OfficeMailer, :type => :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	before(:each) do 
+		ActionMailer::Base.delivery_method = :test
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.deliveries = []
+	end
+
+	after(:each) do 
+		ActionMailer::Base.deliveries.clear
+	end
+
+	describe 'new_office_signup' do 
+		let(:office) {build(:office, email: 'hr@company.com')}
+		let(:mail)   {OfficeMailer.new_office_signup(office)}
+
+
+	end
+
 end
